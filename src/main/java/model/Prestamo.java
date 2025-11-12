639b8b18cd73ae3b10f05c28f0d434939aeec67c
David <davidsatafi@gmail.com>
2025-11-12 10:29:12 +0100

    Primer Commit

--- src/main/java/model/Prestamo.java
+++ src/main/java/model/Prestamo.java
@@ -1,4 +1,72 @@
 package model;
 
+import java.util.Date;
+
 public class Prestamo {
+    private int id;
+    private Date fechaInicio;
+    private Date fechaFin;
+    Usuario usuario;
+    Libro libro;
+    public Prestamo() {
+    }
+
+    public Prestamo(int id, Date fechaInicio, Date fechaFin, Usuario usuario, Libro libro) {
+        this.id = id;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.usuario = usuario;
+        this.libro = libro;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(Date fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public Date getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(Date fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    public Libro getLibro() {
+        return libro;
+    }
+
+    public void setLibro(Libro libro) {
+        this.libro = libro;
+    }
+
+    @Override
+    public String toString() {
+        return "Prestamo{" +
+                "id=" + id +
+                ", fechaInicio=" + fechaInicio +
+                ", fechaFin=" + fechaFin +
+                ", usuario=" + usuario +
+                ", libro=" + libro +
+                '}';
+    }
 }
