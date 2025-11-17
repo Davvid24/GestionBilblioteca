2075ead66481a975c1d743bec83a4dc44788239b
David <davidsatafi@gmail.com>
2025-11-17 16:26:41 +0100

    Cambios Libro_AutorDAO.java

--- src/main/java/dao/Libro_AutorDAO.java
+++ src/main/java/dao/Libro_AutorDAO.java
@@ -1,4 +1,13 @@
 package dao;
 
+import model.Autor;
+import model.Libro;
+
+import java.util.List;
+
 public interface Libro_AutorDAO {
+    void anadirLibroAutor(Autor autor, Libro libro) throws Exception;
+    void eliminarLibroAutor(int id) throws Exception;
+    List<Autor> listarAutoresLibros() throws Exception;
+    void actualizarAutoresLibros(Autor autor, Libro libro) throws Exception;
 }
