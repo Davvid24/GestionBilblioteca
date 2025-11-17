1dd8dabd29cfaecbb93d19f2f238ca653db0d127
David <davidsatafi@gmail.com>
2025-11-17 16:39:31 +0100

    Cambios en Libro_AutorDAOimpl.java, PrestamoDAO.java

--- src/main/java/dao/Libro_AutorDAOimpl.java
+++ src/main/java/dao/Libro_AutorDAOimpl.java
@@ -1,4 +1,28 @@
 package dao;
 
-public class Libro_AutorDAOimpl {
+import model.Autor;
+import model.Libro;
+
+import java.util.List;
+
+public class Libro_AutorDAOimpl implements Libro_AutorDAO {
+    @Override
+    public void anadirLibroAutor(Autor autor, Libro libro) throws Exception {
+
+    }
+
+    @Override
+    public void eliminarLibroAutor(int id) throws Exception {
+
+    }
+
+    @Override
+    public List<Autor> listarAutoresLibros() throws Exception {
+        return List.of();
+    }
+
+    @Override
+    public void actualizarAutoresLibros(Autor autor, Libro libro) throws Exception {
+
+    }
 }
--- src/main/java/dao/PrestamoDAO.java
+++ src/main/java/dao/PrestamoDAO.java
@@ -1,4 +1,14 @@
 package dao;
 
+import model.Autor;
+import model.Prestamo;
+
+import java.util.List;
+
 public interface PrestamoDAO {
+    void anadirPrestamo(Prestamo prestamo) throws Exception;
+    void eliminarPrestamo(int id) throws Exception;
+    List<Autor> listarPrestamo() throws Exception;
+    void actualizarPrestamo(Prestamo prestamo) throws Exception;
+
 }
