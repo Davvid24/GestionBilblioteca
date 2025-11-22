173a3a23b94738b4f5f76847d2a89b983e43b17b
David <davidsatafi@gmail.com>
2025-11-22 12:44:39 +0100

    Cambios en el main, gestion correcta de datos duplicados.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -34,30 +34,26 @@ public class Main {
                          3. Gestionar Prestamos.
                          4. Gestionar Usuarios.
                          5. Salir.
-                         \s
+                         
                           Introduce el nº de una de estas opciones.
-                        \s
-                        \s""");
+                        
+                        
+                        """);
                 int opcion = sc.nextInt();
+                sc.nextLine(); // limpiar buffer
+
                 switch (opcion) {
                     case 1:
-                        //GESTIONAR AUTORES
                         menuAutor(sc, bibliotecaService);
                         break;
                     case 2:
-                        //GESTIONAR AUTORES
                         menuLibro(sc, bibliotecaService);
-
                         break;
                     case 3:
-                        //GESTIONAR AUTORES
                         menuPrestamos(sc, bibliotecaService);
-
                         break;
                     case 4:
-                        //GESTIONAR AUTORES
                         menuUsuarios(sc, bibliotecaService);
-
                         break;
                     case 5:
                         System.out.println("Finalizando gestion.");
@@ -89,53 +85,55 @@ public class Main {
                          3. Actualizar Autor.
                          4. Eliminar Autor.
                          5. Salir.
-                         \s
+                         
                           Introduce el nº de una de estas opciones.
-                        \s
-                        \s""");
-
+                        
+                        
+                        """);
 
                 int opcion = sc.nextInt();
+                sc.nextLine(); // limpiar buffer
+
                 switch (opcion) {
                     case 1:
-                        //añadir libro
                         System.out.println("Introduce los datos del Autor:");
                         System.out.print("Nombre: ");
                         String nombre = sc.nextLine();
                         Autor autor = new Autor(0, nombre);
                         bibliotecaService.anadirAutor(autor);
-
                         break;
+
                     case 2:
                         bibliotecaService.listarAutor();
                         break;
+
                     case 3:
-                        bibliotecaService.listarLibros();
+                        bibliotecaService.listarAutor();
                         System.out.print("Introduce el id del Autor a modificar:");
                         int id = sc.nextInt();
-                        System.out.println();
+                        sc.nextLine(); // limpiar buffer
                         System.out.println("Introduce el nuevo Nombre del Autor");
                         nombre = sc.nextLine();
                         bibliotecaService.actualizarAutor(id, nombre);
                         break;
+
                     case 4:
                         bibliotecaService.listarAutor();
-                        System.out.println("Introduce el id del Autor a elimianr:");
+                        System.out.println("Introduce el id del Autor a eliminar:");
                         id = sc.nextInt();
+                        sc.nextLine(); // limpiar buffer
                         bibliotecaService.eliminaAutor(id);
                         break;
+
                     case 5:
                         control = false;
                         break;
                 }
             }
 
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
     public static void menuLibro(Scanner sc, BibliotecaService bibliotecaService) {
@@ -151,16 +149,17 @@ public class Main {
                          3. Actualizar Libro.
                          4. Eliminar Libro.
                          5. Salir.
-                         \s
+                         
                           Introduce el nº de una de estas opciones.
-                        \s
-                        \s""");
-
+                        
+                        
+                        """);
 
                 int opcion = sc.nextInt();
+                sc.nextLine(); // limpiar buffer
+
                 switch (opcion) {
                     case 1:
-                        //añadir libro
                         System.out.println("Introduce los datos del libro:");
                         System.out.print("Título: ");
                         String titulo = sc.nextLine();
@@ -171,39 +170,46 @@ public class Main {
                         Libro libro = new Libro(0, titulo, isbn);
                         bibliotecaService.anadirLibro(libro);
                         break;
+
                     case 2:
                         bibliotecaService.listarLibros();
                         break;
+
                     case 3:
                         bibliotecaService.listarLibros();
                         System.out.print("Introduce el id del libro a cambiar:");
                         int id = sc.nextInt();
+                        sc.nextLine(); // limpiar buffer
                         System.out.println();
+
                         System.out.println("Introduce el nuevo titulo del libro:");
                         titulo = sc.nextLine();
                         System.out.println();
+
                         System.out.println("Introduce el nuevo isbn del libro:");
                         isbn = sc.nextLine();
                         System.out.println();
+
                         bibliotecaService.actualizarLibro(id, titulo, isbn);
                         break;
+
                     case 4:
                         bibliotecaService.listarLibros();
-                        System.out.println("Introduce el id del libro a elimianr:");
+                        System.out.println("Introduce el id del libro a eliminar:");
                         id = sc.nextInt();
+                        sc.nextLine(); // limpiar buffer
                         bibliotecaService.eliminarLibro(id);
                         break;
+
                     case 5:
                         control = false;
                         break;
                 }
             }
 
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
 
     }
 
@@ -220,76 +226,91 @@ public class Main {
                          3. Actualizar Préstamo.
                          4. Eliminar Préstamo.
                          5. Salir.
-                         \s
+                         
                           Introduce el nº de una de estas opciones.
-                        \s
-                        \s""");
-
+                        
+                        
+                        """);
 
                 int opcion = sc.nextInt();
+                sc.nextLine(); // limpiar buffer
+
                 switch (opcion) {
                     case 1:
-                        //añadir prestamo
                         System.out.println("Introduce los datos del préstamo:");
-                        System.out.print("Fecha inicio: ");
+                        System.out.print("Fecha inicio (YYYY-MM-DD): ");
                         Date fechaInicio = Date.valueOf(sc.nextLine());
                         System.out.println();
-                        System.out.print("Fecha Fin: ");
+
+                        System.out.print("Fecha Fin (YYYY-MM-DD): ");
                         Date fechaFin = Date.valueOf(sc.nextLine());
                         System.out.println();
+
                         System.out.print("Id del Usuario: ");
                         int idUsuario = sc.nextInt();
-                        System.out.println();
-                        System.out.print("Id del Libro: ");
-                        int idLibro = sc.nextInt();
+                        sc.nextLine();
                         System.out.println();
 
-                        Prestamo prestamo = new Prestamo(0,  fechaInicio, fechaFin, idUsuario, idLibro);
+                        System.out.print("Id del Libro: ");
+                        int idLibro = sc.nextInt();
+                        sc.nextLine();
+                        System.out.println();
+
+                        Prestamo prestamo = new Prestamo(0, fechaInicio, fechaFin, idUsuario, idLibro);
                         bibliotecaService.anadirPrestamo(prestamo);
                         break;
+
                     case 2:
                         bibliotecaService.listarPrestamos();
                         break;
+
                     case 3:
                         bibliotecaService.listarPrestamos();
                         System.out.print("Introduce el id del préstamo a cambiar:");
                         int id = sc.nextInt();
+                        sc.nextLine();
                         System.out.println();
-                        System.out.print("nueva Fecha inicio: ");
+
+                        System.out.print("Nueva Fecha inicio (YYYY-MM-DD): ");
                         fechaInicio = Date.valueOf(sc.nextLine());
                         System.out.println();
-                        System.out.print("nueva Fecha Fin: ");
+
+                        System.out.print("Nueva Fecha Fin (YYYY-MM-DD): ");
                         fechaFin = Date.valueOf(sc.nextLine());
                         System.out.println();
+
                         System.out.print("Id del nuevo Usuario: ");
                         idUsuario = sc.nextInt();
+                        sc.nextLine();
                         System.out.println();
+
                         System.out.print("Id del nuevo Libro: ");
                         idLibro = sc.nextInt();
+                        sc.nextLine();
                         System.out.println();
-                        prestamo = new Prestamo(id,  fechaInicio, fechaFin, idUsuario, idLibro);
+
+                        prestamo = new Prestamo(id, fechaInicio, fechaFin, idUsuario, idLibro);
                         bibliotecaService.actualizarPrestamo(prestamo);
 
-
                         break;
+
                     case 4:
                         bibliotecaService.listarPrestamos();
                         System.out.println("Introduce el id del préstamo a eliminar:");
                         id = sc.nextInt();
+                        sc.nextLine();
                         bibliotecaService.eliminarPrestamo(id);
                         break;
+
                     case 5:
                         control = false;
                         break;
                 }
             }
 
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
-
     }
 
     public static void menuUsuarios(Scanner sc, BibliotecaService bibliotecaService) {
@@ -305,53 +326,57 @@ public class Main {
                          3. Actualizar Usuario.
                          4. Eliminar Usuario.
                          5. Salir.
-                         \s
+                         
                           Introduce el nº de una de estas opciones.
-                        \s
-                        \s""");
-
+                        
+                        
+                        """);
 
                 int opcion = sc.nextInt();
+                sc.nextLine(); // limpiar buffer
+
                 switch (opcion) {
                     case 1:
-                        //añadir
-                        System.out.println("Introduce el nombre del Usuario:");
+                        System.out.println("Introduce los datos del Usuario:");
                         System.out.print("Nombre: ");
                         String nombre = sc.nextLine();
                         System.out.println();
                         Usuario usuario = new Usuario(0, nombre);
                         bibliotecaService.anadirUsuario(usuario);
                         break;
+
                     case 2:
-                        bibliotecaService.listarUsuario ();
+                        bibliotecaService.listarUsuario();
                         break;
+
                     case 3:
                         bibliotecaService.listarUsuario();
                         System.out.print("Introduce el id del Usuario a cambiar:");
                         int id = sc.nextInt();
+                        sc.nextLine();
                         System.out.println("Introduce el nuevo Nombre del Usuario:");
                         nombre = sc.nextLine();
                         System.out.println();
-
                         bibliotecaService.actualizarUsuario(id, nombre);
                         break;
+
                     case 4:
-                        bibliotecaService.listarLibros();
+                        bibliotecaService.listarUsuario();
                         System.out.println("Introduce el id del usuario a eliminar:");
                         id = sc.nextInt();
+                        sc.nextLine();
                         bibliotecaService.eliminarUsuario(id);
                         break;
+
                     case 5:
                         control = false;
                         break;
                 }
             }
 
-
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-
 
     }
 }
